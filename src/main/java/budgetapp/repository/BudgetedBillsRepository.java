b20d627385548584bd1c4d21615e0e6dd80855b8
Kelly Klein <kmklein2@dmacc.edu>
2020-04-14 20:27:48 -0500

    fix  attempt

--- src/main/java/budgetapp/repository/BudgetedBillsRepository.java
+++
@@ -1,9 +0,0 @@
-package budgetapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import budgetapp.beans.BudgetedBills;
-
-public interface BudgetedBillsRepository extends JpaRepository<BudgetedBills, Long>{
-	
-	}

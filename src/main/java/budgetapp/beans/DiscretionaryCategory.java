b87480a19b1df16bff457dd4d26da853703f0850
cgonz1286 <60245968+cgonz1286@users.noreply.github.com>
2020-04-14 21:09:48 -0500

    Added DiscretionaryCategory entity

--- src/main/java/budgetapp/beans/DiscretionaryCategory.java
+++ src/main/java/budgetapp/beans/DiscretionaryCategory.java
@@ -1,5 +1,55 @@
 package budgetapp.beans;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="DiscretionaryCategory")
 public class DiscretionaryCategory {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String description;
+	
+	public DiscretionaryCategory() {
+		super();
+		this.description = "";
+	}
+	
+	public DiscretionaryCategory(String description) {
+		super();
+		this.description = description;
+	}
+	
+	public DiscretionaryCategory(long id, String description) {
+		super();
+		this.id = id;
+		this.description = description;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "DiscretionaryCategory [id=" + id + ", description=" + description + "]";
+	}
+	
 }
